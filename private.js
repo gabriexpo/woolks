36d99507f26b6b509efe889f1e8b0276b6763912
gabriexpo <gabriexpo@libero.it>
2022-11-15 16:06:31 +0100

    added private to remote repo

---
+++ private.js
@@ -0,0 +1,12 @@
+const links = [
+    ["Gogs", "http://192.168.1.151:3000"],
+    ["GitHub", "https://github.com"],
+    ["Mangaread", "https://www.mangaread.org"],
+    ["Mangakomi", "https://mangakomi.com"],
+    ["Webtoon", "https://webtoons.com/en"],
+    ["Disney+", "https://www.disneyplus.com/it-it/home"],
+    ["Netflix", "https://www.netflix.com/browse"],
+    ["Prime Video", "https://www.primevideo.com"]
+]
+
+const name = "Deviny"
