fd2edc92b1955919b53911625cd70f428a0b7eb7
gabriexpo <gabriexpo@libero.it>
2022-11-15 16:25:11 +0100

    moving bookmarks

--- private.js
+++ private.js
@@ -1,12 +1,12 @@
 const links = [
-    ["Gogs", "http://192.168.1.151:3000"],
-    ["GitHub", "https://github.com"],
+    ["Disney+", "https://www.disneyplus.com/it-it/home"],
     ["Mangaread", "https://www.mangaread.org"],
     ["Mangakomi", "https://mangakomi.com"],
-    ["Webtoon", "https://webtoons.com/en"],
-    ["Disney+", "https://www.disneyplus.com/it-it/home"],
+    ["Gogs", "http://192.168.1.151:3000"],
+    ["Prime Video", "https://www.primevideo.com"],
     ["Netflix", "https://www.netflix.com/browse"],
-    ["Prime Video", "https://www.primevideo.com"]
+    ["Webtoon", "https://webtoons.com/en"],
+    ["GitHub", "https://github.com"]
 ]
 
 const name = "Deviny"
